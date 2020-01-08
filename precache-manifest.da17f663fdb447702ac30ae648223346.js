a09f77733ae98bed4bf56aeef6d9d1224e5c2a12
yourtechsis <aretee64@gmail.com>
2020-01-08 14:12:46 -0700

    Updates

---
+++ precache-manifest.da17f663fdb447702ac30ae648223346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5badc505bd2159cda86e9455da2a6d0",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "b0f0bc071fb651588432",
+    "url": "/redux/static/css/main.bfe30c8f.chunk.css"
+  },
+  {
+    "revision": "a410931127b8591cae88",
+    "url": "/redux/static/js/2.3f30025a.chunk.js"
+  },
+  {
+    "revision": "3d288c697acc5e47e019d9c118e254a6",
+    "url": "/redux/static/js/2.3f30025a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0f0bc071fb651588432",
+    "url": "/redux/static/js/main.9d9e166d.chunk.js"
+  },
+  {
+    "revision": "923971c6e00fb6c199a3",
+    "url": "/redux/static/js/runtime-main.872a3610.js"
+  },
+  {
+    "revision": "abdc77c731d33342631130a62f19ac72",
+    "url": "/redux/static/media/icon4.abdc77c7.svg"
+  }
+]);
